72f3340d0f5c343411e33ed26f39d635b90d3c84
RuzlaJabir <154322240+RuzlaJabir@users.noreply.github.com>
2025-03-16 06:35:02 +0000

    Q04-Print odd numbers between 1 to 20 added

---
+++ Q04_PrintOdd.java
@@ -0,0 +1,11 @@
+public class Q04_PrintOdd{
+    public static void main(String args[]){
+
+        for(int i=1; i<=20; i++){
+            if(i % 2 == 1)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
