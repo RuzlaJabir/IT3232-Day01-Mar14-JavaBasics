a139382888bb42b5bac239061f2791b038b2ceb1
RuzlaJabir <154322240+RuzlaJabir@users.noreply.github.com>
2025-03-16 08:29:01 +0000

    Q07-Created the child class AudioBook

---
+++ Q07/AudioBook.java
@@ -0,0 +1,23 @@
+public class AudioBook extends Book {
+    private double duration;
+    private String narrator;
+
+    public AudioBook(String title, String author, int publicationYear, double duration, String narrator) {
+        super(title, author, publicationYear);
+        this.duration = duration;
+        this.narrator = narrator;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public String getNarrator() {
+        return narrator;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Audio Book: " + getTitle() + " by " + getAuthor() + ", Year: " + getPublishedYear() + ", Duration: " + duration + " hours, Narrated by: " + narrator);
+    }
+}
